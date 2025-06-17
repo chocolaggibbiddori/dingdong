c00c88628c51b73ed13aa5f0663e36fc1899421d
chocolaggibbiddori <didrkd684@naver.com>
2025-06-17 16:18:17 +0900

    init api project

---
+++ backend/api/src/test/java/side/dingdong/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package side.dingdong.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
