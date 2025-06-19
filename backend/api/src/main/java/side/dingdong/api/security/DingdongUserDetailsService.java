2cbd00c1cf7e937f07cd369fd09df2fc7a9d9113
chocolaggibbiddori <didrkd684@naver.com>
2025-06-19 23:25:30 +0900

    [Fix] DingdongUserDetailsService - 트랜잭션 미설정 버그

--- backend/api/src/main/java/side/dingdong/api/security/DingdongUserDetailsService.java
+++ backend/api/src/main/java/side/dingdong/api/security/DingdongUserDetailsService.java
@@ -6,10 +6,12 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import side.dingdong.api.domain.user.UserRepository;
 import side.dingdong.api.domain.user.entity.User;
 
 @Service
+@Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class DingdongUserDetailsService implements UserDetailsService {
 
