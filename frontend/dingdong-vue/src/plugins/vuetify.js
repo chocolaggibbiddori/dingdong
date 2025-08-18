b000d797a2ee779f8cf78ce022b20c14af4799eb
chocolaggibbiddori <didrkd684@naver.com>
2025-08-18 18:37:03 +0900

    [Vue, Config] prettier, vuetify 설정 변경

--- frontend/dingdong-vue/src/plugins/vuetify.js
+++ frontend/dingdong-vue/src/plugins/vuetify.js
@@ -1,14 +1,23 @@
-import 'vuetify/styles'
-import { createVuetify } from 'vuetify'
-import { aliases, mdi } from 'vuetify/iconsets/mdi'
+import 'vuetify/styles';
+import '@mdi/font/css/materialdesignicons.css';
+import { createVuetify } from 'vuetify';
+import { aliases, mdi } from 'vuetify/iconsets/mdi';
+import * as components from 'vuetify/components';
+import * as directives from 'vuetify/directives';
+import * as labs from 'vuetify/labs/components';
 
 export const vuetify = createVuetify({
-	icons: {
-		defaultSet: 'mdi',
-		aliases,
-		sets: { mdi }
-	},
-	theme: {
-		defaultTheme: 'light'
-	}
-})
+  components: {
+    ...components,
+    ...labs
+  },
+  directives,
+  icons: {
+    defaultSet: 'mdi',
+    aliases,
+    sets: { mdi }
+  },
+  theme: {
+    defaultTheme: 'light'
+  }
+});
