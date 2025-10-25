4f22e2f1a2dfa1ab434486059801761f063cbe36
chocolaggibbiddori <didrkd684@naver.com>
2025-10-26 01:49:11 +0900

    [Vue, Feat] Alert modal

---
+++ frontend/dingdong-vue/src/components/modal/AlertModal.vue
@@ -0,0 +1,40 @@
+<script setup>
+import BaseModal from '@/components/modal/BaseModal.vue';
+
+const open = defineModel({
+  type: Boolean,
+  default: false
+});
+
+defineProps({
+  title: {
+    type: String,
+    default: '안내'
+  },
+  message: {
+    type: String,
+    required: true
+  }
+});
+</script>
+
+<template>
+  <base-modal
+    v-model="open"
+    :title
+  >
+    {{ message }}
+
+    <template #actions>
+      <v-btn
+        color="primary"
+        variant="flat"
+        @click="open = false"
+      >
+        확인
+      </v-btn>
+    </template>
+  </base-modal>
+</template>
+
+<style scoped></style>
