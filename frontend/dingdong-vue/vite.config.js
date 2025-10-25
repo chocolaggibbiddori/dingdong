9ff16567d0b0bef13875c5a78f9a924487df47ed
chocolaggibbiddori <didrkd684@naver.com>
2025-10-26 02:10:47 +0900

    [Vue, Config] Upgrade vite.config.js

--- frontend/dingdong-vue/vite.config.js
+++ frontend/dingdong-vue/vite.config.js
@@ -4,16 +4,18 @@ import vue from '@vitejs/plugin-vue';
 import vueDevTools from 'vite-plugin-vue-devtools';
 
 // https://vite.dev/config/
-export default defineConfig({
-	plugins: [vue(), vueDevTools()],
-	resolve: {
-		alias: {
-			'@': fileURLToPath(new URL('./src', import.meta.url))
-		}
-	},
-	server: {
-		hmr: {
-			overlay: false
-		}
-	}
+export default defineConfig(({ command }) => {
+  return {
+    plugins: [vue(), command === 'serve' && vueDevTools()],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      }
+    },
+    server: {
+      hmr: {
+        overlay: false
+      }
+    }
+  };
 });
